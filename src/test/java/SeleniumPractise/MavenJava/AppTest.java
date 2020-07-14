8af61fc8d8f3dac51b337a933dfa4cdebed6da7e
sharath <ksharathyadav@gmail.com>
2020-07-14 11:09:42 +0530

    my git test

---
+++ src/test/java/SeleniumPractise/MavenJava/AppTest.java
@@ -0,0 +1,16 @@
+package SeleniumPractise.MavenJava;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+    @Test
+    public void AppTest()
+    {
+        System.out.println("Hi This Maven Java Selenium");
+    }
+}
